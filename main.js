6b5d80dff3c439d6525be02db5cf8bf2948a9017
nischalgurung <nizallgurung16@gmail.com>
2021-12-24 13:30:32 +0530

    Added  prev and next buttons

--- main.js
+++ main.js
@@ -1,27 +1,35 @@
-const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=45232bdb518489d70b10d7934daaab45&page=1';
+const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=45232bdb518489d70b10d7934daaab45&page=';
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=45232bdb518489d70b10d7934daaab45&query="';
 
-const main = document.querySelector('#main');
 const form = document.querySelector('#form');
 const search = document.querySelector('#search');
+const main = document.querySelector('#main');
+const nextBtn = document.querySelector('#next');
+const prevBtn = document.querySelector('#prev');
 
 // Get initial movies
 getMovies(API_URL);
+
 function getMovies(url) {
     fetch(url)
-  .then(response => response.json())
-  .then(data => showMovies(data.results));
-    
+        .then(response => response.json())
+        .then(data => showMovies(data.results));
+
 }
 
 function showMovies(movies) {
     main.innerHTML = '';
     movies.forEach((movie) => {
-        const { title, poster_path, vote_average, overview } = movie;
+        const {
+            title,
+            poster_path,
+            vote_average,
+            overview
+        } = movie;
         const movieEl = document.createElement('div');
         movieEl.classList.add('movie');
-        movieEl.innerHTML=`
+        movieEl.innerHTML = `
             <img src="${IMG_PATH + poster_path}" alt="${title}">
             <div class="movie-info">
                 <h3>${title}</h3>
@@ -33,21 +41,19 @@ function showMovies(movies) {
             </div>
         `
         main.appendChild(movieEl);
-        
+
     });
 }
 
 function getClassByRate(vote) {
     if (vote >= 8) {
         return 'green';
-    }
-    else if (vote >= 5) {
+    } else if (vote >= 5) {
         return 'orange';
-    }
-    else {
+    } else {
         return 'red';
     }
-    
+
 }
 
 form.addEventListener('submit', (evt) => {
@@ -56,9 +62,28 @@ form.addEventListener('submit', (evt) => {
     if (searchTerm && searchTerm != '') {
         getMovies(SEARCH_API + searchTerm);
         search.value = '';
-    }
-    else {
+    } else {
         window.location.reload();
     }
 })
 
+let i = 1;
+
+prevBtn.addEventListener('click', function () {
+    i = --i;
+    getMovies(API_URL + i);
+    window.scrollTo(0, 0);
+    if (i <= 1) {
+        prevBtn.classList.add('disabled');
+        prevBtn.disabled = true;
+    }
+});
+nextBtn.addEventListener('click', function () {
+    i = ++i;
+    getMovies(API_URL + i);
+    window.scrollTo(0, 0);
+    if (i > 1) {
+        prevBtn.classList.remove('disabled');
+        prevBtn.disabled = false;
+    }
+});
